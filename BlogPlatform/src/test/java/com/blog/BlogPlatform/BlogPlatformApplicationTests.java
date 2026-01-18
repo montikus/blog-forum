132be2e9ff3b7f7294a086fdd6ad5656c789f588
Roman_Nadkernychnyi <r.nadkernychnyi.972@studms.ug.edu.pl>
2026-01-18 18:48:30 +0100

    bluebrint

---
+++ BlogPlatform/src/test/java/com/blog/BlogPlatform/BlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.BlogPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
